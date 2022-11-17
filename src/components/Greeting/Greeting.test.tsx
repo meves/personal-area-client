9176acc939cb18ff885baca0c063284ae7f783d2
Sergey Medvedkin <meves.sergey@gmail.com>
2022-11-17 18:44:45 +0300

    Greeting component tested

--- src/components/Greeting/Greeting.test.tsx
+++ src/components/Greeting/Greeting.test.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-// API mocking utilities
-import { rest } from "msw";
-import { setupServer } from "msw/node";
 // react testing methods
-import { render, screen, waitFor } from "@testing-library/react"
+import { act, render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event";
 // custom jest matcher from jest-dom
 import "@testing-library/jest-dom";
@@ -11,78 +8,82 @@ import "@testing-library/jest-dom";
 import { Greeting } from ".";
 // import hooks to mock
 import * as useHooks from "../../app/hooks";
-import { store } from "../../store";
 
+jest.mock("../../app/hooks");
+jest.mock("../../store/greetingSlice");
+
+
+const mockUseAppSelector = jest.spyOn(useHooks, "useAppSelector");
 
 afterEach(() => {
     jest.restoreAllMocks()
 })
 
-describe("render correct UI", () => {
-    test("When the component is mounted, a button with the text 'Load greeting' is displayed.", 
-    () => {
-        // ARRANGE
-        // create mock functions
-        jest.spyOn(useHooks, "useAppDispatch").mockReturnValue(store.dispatch);
-        jest.spyOn(useHooks, "useAppSelector").mockReturnValue(null);
-        // ACT
+describe("render correct UI", () => {    
+    test("first render", async () => {
+        // ** ARRANGE
+        const dispatch = jest.fn();
+        jest.spyOn(useHooks, "useAppDispatch").mockReturnValue(dispatch);
+        mockUseAppSelector.mockReturnValue(null);
+        // ** ACT
         const { container } = render(<Greeting/>, {
             container: document.body.appendChild(document.createElement("div"))
         })
-        // ASSERT
-        expect(screen.getByRole("button")).toHaveTextContent("Load greeting");
-        expect(screen.getByText("Load greeting")).toBeVisible();
-        try {
-            expect(screen.getByRole("heading")).toBeVisible();
-        } catch (error) {
-            expect(error).toBe(error);
-        }
-        try {
-            expect(screen.getByRole("alert")).toBeVisible();
-        } catch (error) {
-            expect(error).toBe(error);
-        }
-        // p is not displayed
+        // ** ASSERT
+        const button = screen.getByRole("button");
+        expect(button).toBeInTheDocument();
+        expect(button).toHaveTextContent("Load greeting");
+        expect(screen.queryByRole("heading")).not.toBeInTheDocument();
+        expect(screen.queryByRole("alert")).not.toBeInTheDocument();        
+    })
+
+    test("click on button results in success", async () => {
+        // ** ARRANGE
+        const dispatch: any = jest.fn(() => true);
+        jest.spyOn(useHooks, "useAppDispatch").mockReturnValue(dispatch);
+        mockUseAppSelector.mockReturnValue("Hello");
+        const mockOnClick = jest.fn();
+        // ** ACT
+        const { container } = render(<Greeting/>, {
+            container: document.body.appendChild(document.createElement("div"))
+        })
+        const button = screen.getByRole("button");
+        button.onclick = mockOnClick;
+        await act(async () => {
+            userEvent.click(button);
+        })
+        // ** ASSERT
+        // button
+        expect(mockOnClick).toBeCalledTimes(1);
+        expect(button).toHaveTextContent("OK");
+        // heading
+        const heading = screen.getByRole("heading");
+        expect(heading).toBeInTheDocument();
+        expect(heading).toHaveTextContent("Hello");
+    })
+
+    test("click on button results in an error", async () => {
+        // ** ARRANGE
+        mockUseAppSelector.mockReturnValue("Internal server error");
+        const dispatch: any = jest.fn(() => false);
+        jest.spyOn(useHooks, "useAppDispatch").mockReturnValue(dispatch);
+        const mockOnClick = jest.fn();
+        // ** ACT
+        const { container } = render(<Greeting/>, {
+            container: document.body.appendChild(document.createElement("div"))
+        })
+        const button = screen.getByRole("button");
+        button.onclick = mockOnClick;
+        await act(async () => {
+            userEvent.click(button);
+        })
+        // ** ASSERT
+        // button
+        expect(mockOnClick).toBeCalledTimes(1);
+        expect(button).toHaveTextContent("Load greeting");
+        // alert
+        const alert = screen.getByRole("alert");
+        expect(alert).toBeInTheDocument();
+        expect(alert).toHaveTextContent("Oops, failed to fetch");        
     })
 })
-
-// define API request to mock
-// const server = setupServer(
-//     // capture GET /api/greeting/:id requests
-//     rest.get("/api/greeting/:id", (req, res, ctx) => {
-//         return res(ctx.json({ data: {greeting: "Hello there"} }))
-//     })
-// );
-
-// beforeAll(() => server.listen());
-// beforeEach(() => server.resetHandlers());
-// afterAll(() => server.close());
-
-// describe("requests to server", () => {
-//     // test("loads and displays greeting", async () => {
-//     //     // Arrange
-//     //     render(<Greeting/>);
-//     //     // Act
-//     //     await userEvent.click(screen.getByText("Load greeting"));
-//     //     await screen.findByRole("heading");
-//     //     // Assert
-//     //     expect(screen.getByRole("heading")).toHaveTextContent("Hello there");
-//     //     expect(screen.getByRole("button")).toBeDisabled();
-//     // });
-    
-//     // test("handles server error", async () => {
-//     //     // Arrange
-//     //     server.use(
-//     //         rest.get("/api/greeting/:id", (req, res, ctx) => {
-//     //             return res(ctx.status(500));
-//     //         })
-//     //     );
-//     //     render(<Greeting/>);
-//     //     // Act
-//     //     await userEvent.click(screen.getByText("Load greeting"));
-//     //     await screen.findByRole("alert");
-//     //     // Assert
-//     //     expect(screen.getByRole("alert")).toHaveTextContent("Oops, failed to fetch");
-//     //     expect(screen.getByRole("button")).toBeDisabled();
-//     // });
-// });
